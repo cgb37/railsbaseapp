bfeaa975fd890f6675b622ae401b6a398f4ae023
Charles <cgb37@users.noreply.github.com>
2016-10-04 17:16:42 -0400

    update template

--- template.rb
+++ template.rb
@@ -8,7 +8,7 @@ def apply_template!
   #ui bootstrap
   gem 'bootstrap-sass'
 
-  gem 'activeadmin', github: 'activeadmin'
+  gem 'activeadmin', github: 'activeadmin/activeadmin'
 
   # activeadmin dependencies
   # authentication
@@ -27,13 +27,6 @@ def apply_template!
   #authorization
   gem 'cancancan'
 
-  #search
-  gem 'ransack'
-  # end activeadmin dependencies
-
-  # user authorization
-  gem 'cancancan'
-
   # db seeder
   gem 'faker'
 
@@ -43,7 +36,7 @@ def apply_template!
 
 
   # Use active_admin_import for csv import - https://github.com/activeadmin-plugins/active_admin_import
-  gem 'active_admin_import' , github: 'Fivell/active_admin_import'
+  gem 'active_admin_import', github: "activeadmin-plugins/active_admin_import"
 
   # use active record store to prevent cookie overflow issue when importing
   # https://github.com/rails/activerecord-session_store
@@ -56,10 +49,13 @@ def apply_template!
   #inline edit
   gem 'best_in_place'
 
-
+  # font icons
+  gem 'font-awesome-sass', '~> 4.6.2'
 
 
   #callbacks
+  bundle_install
+
   run_generators
 
   remove_files
@@ -71,7 +67,6 @@ def apply_template!
   rake_migrations
 
 
-
 end
 
 
@@ -80,49 +75,43 @@ def source_paths
   [File.expand_path(File.dirname(__FILE__))]
 end
 
+def bundle_install
+  run 'bundle install'
+end
 
 def remove_files
-
-  remove_file('app/views/layouts/application.html.erb')
-  remove_file('app/views/layouts/_header.html.erb')
-
   remove_file('app/assets/stylesheets/application.css')
-
+  remove_file('app/views/layouts/application.html.erb')
 end
 
 
 def copy_files
-
   copy_file('app/templates/app_copy_src/views/layouts/application.html.erb', 'app/views/layouts/application.html.erb')
   copy_file('app/templates/app_copy_src/views/layouts/_header.html.erb', 'app/views/layouts/_header.html.erb')
+  copy_file('app/templates/app_copy_src/views/layouts/_header.html.erb', 'app/views/layouts/_footer.html.erb')
   copy_file('app/templates/app_copy_src/assets/stylesheets/application.scss', 'app/assets/stylesheets/application.scss')
-  copy_file('app/templates/app_copy_src/assets/stylesheets/bootstrap_overrides.scss', 'app/assets/stylesheets/bootstrap_overrides.scss')
-
+  copy_file('app/templates/app_copy_src/assets/stylesheets/bootstrap-overrides.scss', 'app/assets/stylesheets/bootstrap-overrides.scss')
 end
 
 
 def run_generators
-
   generate(:controller, 'Home', 'index')
-
-  #generate('active_admin:install')
-
-  #generate('devise:views')
-
+  generate('active_admin:install')
+  generate('devise:views')
+  generate('kaminari:config')
+  generate('kaminari:views', 'bootstrap3')
 end
 
 
 def set_routes
-
-  #route "root to: 'home#index'"
+  route "root to: 'home#index'"
 end
 
 
 def rake_migrations
-
-  #rake('db:migrate')
-  #rake('db:seed')
-
+  rake('db:create')
+  rake('db:migrate')
+  rake('db:seed')
 end
 
 
